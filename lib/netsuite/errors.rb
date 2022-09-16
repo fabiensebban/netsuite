8c9b944a502c8f0d0f404b97b42a78badba675ee
fabiensebban <fa.sebban@gmail.com>
2022-09-16 22:34:08 +0200

    Initialization error class

--- lib/netsuite/errors.rb
+++ lib/netsuite/errors.rb
@@ -1,7 +1,14 @@
 module NetSuite
   class RecordNotFound < StandardError; end
-  class InitializationError < StandardError; end
   class ConfigurationError < StandardError; end
+  class InitializationError < StandardError
+    attr_accessor :response
+
+    def initialize(message = "Initialization Error", response = nil)
+      @response = response
+      super(message)
+    end
+  end
 
   # NOTE not an exception, used as a wrapped around NetSuite SOAP error
   class Error
